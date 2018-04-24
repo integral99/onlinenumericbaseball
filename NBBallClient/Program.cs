1cf2dc9e88e4b6a5df6c2fb2075aeee07ee3a2ac
인테그럴 <37678790+integral99@users.noreply.github.com>
2018-04-24 15:15:37 +0900

    Add files via upload
    Console Project 생성
    
    [socket을 다루려면 C#의 socket 선언 구조를 더 알아야 겠다.]

---
+++ NBBallClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace NBBallClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
