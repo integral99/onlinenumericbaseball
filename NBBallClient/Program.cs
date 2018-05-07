587c11c7419a1bc71b7d47cfd088341daa7b3a1c
민혁 전 <jmh121463@gmail.com>
2018-05-08 00:46:31 +0900

    Changes to be committed: 	modified:   NBBallClient/Program.cs 	modified:   NBBallClient/obj/Debug/netcoreapp2.0/NBBallClient.AssemblyInfo.cs 	modified:   NBBallClient/obj/Debug/netcoreapp2.0/NBBallClient.csproj.CoreCompileInputs.cache 	modified:   NBBallClient/obj/NBBallClient.csproj.nuget.cache 	modified:   NBBallClient/obj/NBBallClient.csproj.nuget.g.props 	modified:   NBBallClient/obj/project.assets.json 	modified:   NBBallServer/Program.cs 	modified:   NBBallServer/obj/NBBallServer.csproj.nuget.cache 	modified:   NBBallServer/obj/NBBallServer.csproj.nuget.g.props 	modified:   NBBallServer/obj/project.assets.json
     Untracked files:
    	NBBallClient/.vs/
    	NBBallClient/NBBallClient.sln
    	NBBallClient/bin/
    	NBBallClient/obj/Debug/netcoreapp2.0/NBBallClient.csproj.FileListAbsolute.txt
    	NBBallClient/obj/Debug/netcoreapp2.0/NBBallClient.csprojResolveAssemblyReference.cache
    	NBBallClient/obj/Debug/netcoreapp2.0/NBBallClient.dll
    	NBBallClient/obj/Debug/netcoreapp2.0/NBBallClient.pdb
    	NBBallServer/.vs/
    	NBBallServer/NBBallServer.sln
    	NBBallServer/bin/
    	NBBallServer/obj/Debug/

--- NBBallClient/Program.cs
+++ NBBallClient/Program.cs
@@ -1,14 +1,81 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Net;
-
+using System.Text;
 namespace NBBallClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            string hostIP;
+            int hostPort;
+            Console.Write("Input host's IP in string : ");
+            hostIP = Console.ReadLine();
+            Console.Write("Input host's Port in integer : ");
+            hostPort = int.Parse(Console.ReadLine());
+            NetworkHandler_client clientHandler = 
+                new NetworkHandler_client(hostIP, hostPort);
+            Console.WriteLine("connecting {0}:{1}", hostIP, hostPort);
+            if(clientHandler.Connect() == 0)
+                Console.WriteLine("connected!");
+            while (true)
+            {
+                clientHandler.Write(Console.ReadLine());
+                Console.WriteLine(clientHandler.Read());
+            }
         }
+    }
+    class NetworkHandler_client
+    {
+        private IPAddress hostIP;
+        private TcpClient tcpClient = new TcpClient();
+        private int hostPort;
+        private NetworkStream networkStream;
+        private byte[] r_buffer = new byte[1024];
+        private byte[] w_buffer = new byte[1024];
+        public NetworkHandler_client(string ip, int port)
+        {
+            hostIP = IPAddress.Parse(ip);
+            hostPort = port;
+        }
+        public int Connect()
+        {
+            try
+            {
+                tcpClient.Connect(hostIP, hostPort);
+                networkStream = tcpClient.GetStream();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return 1;
+            }
+            return 0;
+        }
+        public string Read()
+        {
+            networkStream.Read(r_buffer, 0, r_buffer.Length);
+            return Encoding.Default.GetString(r_buffer).Trim('\0');
+        }
+        public int Write(string writeContent)
+        {
+            try
+            {
+                w_buffer = Encoding.UTF8.GetBytes(writeContent);
+                networkStream.Write(w_buffer, 0, w_buffer.Length);
+            }
+            catch(Exception e)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public void Close()
+        {
+            networkStream.Close();
+            tcpClient.Close();
+        }
+
     }
 }
--- NBBallServer/Program.cs
+++ NBBallServer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace NBBallServer
 {
@@ -6,7 +9,71 @@ namespace NBBallServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int hostPort;
+            Console.Write("Input listening port number in integer : ");
+            hostPort = int.Parse(Console.ReadLine());
+            NetworkHandler_server serverHandler = 
+                new NetworkHandler_server(hostPort);
+            Console.WriteLine("Listening on {0}", hostPort);
+            if(serverHandler.Listen() == 0)
+                Console.WriteLine("Connected!");
+            while (true)
+            {
+                serverHandler.Write(Console.ReadLine());
+                Console.WriteLine(serverHandler.Read());
+            }
+        }
+    }
+    class NetworkHandler_server
+    {
+        private IPAddress clientIP;
+        private TcpListener tcpListener;
+        private TcpClient tcpClient;
+        private int listenPort;
+        private NetworkStream networkStream;
+        private byte[] r_buffer = new byte[1024];
+        private byte[] w_buffer = new byte[1024];
+        public NetworkHandler_server(int port)
+        {
+            tcpListener = new TcpListener(IPAddress.Any, port);
+            tcpListener.Start();
+        }
+        public int Listen()
+        {
+            try
+            {
+                tcpClient = tcpListener.AcceptTcpClient();
+                networkStream = tcpClient.GetStream();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return 1;
+            }
+            return 0;
+        }
+        public string Read()
+        {
+            int len = networkStream.Read(r_buffer, 0, r_buffer.Length);
+            return Encoding.Default.GetString(r_buffer).Trim('\0');
+        }
+        public int Write(string writeContent)
+        {
+            try
+            {
+                w_buffer = Encoding.UTF8.GetBytes(writeContent);
+                networkStream.Write(w_buffer, 0, w_buffer.Length);
+            }
+            catch (Exception e)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public void Close()
+        {
+            networkStream.Close();
+            tcpClient.Close();
         }
     }
 }
